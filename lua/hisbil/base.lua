1b237ca49d8c9f2bfba7e6b8689963238d5caaa1
hisbil islami <hisbil.prog@gmail.com>
2022-09-10 22:33:43 +0700

    update

--- lua/hisbil/base.lua
+++ lua/hisbil/base.lua
@@ -48,8 +48,32 @@ vim.g.mapleader = ' '
 vim.api.nvim_create_autocmd('FileType', {
     pattern = "php",
     callback = function()
-      vim.opt.shiftwidth = 4
-      vim.opt.tabstop = 4
-      vim.opt.autoindent = true
+      vim.opt_local.shiftwidth = 4
+      vim.opt_local.tabstop = 4
+      vim.opt_local.autoindent = true
+    end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = "coffee",
+    callback = function()
+      vim.opt_local.shiftwidth = 2
+      vim.opt_local.tabstop = 2
+    end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = "ruby",
+    callback = function()
+      vim.opt_local.shiftwidth = 2
+      vim.opt_local.tabstop = 2
+    end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = "yaml",
+    callback = function()
+      vim.opt_local.shiftwidth = 2
+      vim.opt_local.tabstop = 2
     end,
 })

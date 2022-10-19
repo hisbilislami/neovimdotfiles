8554b050f4cdfec90afd14eb5ecc9136de11c901
hisbil islami <hisbil.prog@gmail.com>
2022-10-19 16:43:55 +0700

    update

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -219,6 +219,11 @@ _G.packer_plugins = {
     path = "/home/hisbil/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
+  ["vim-fugitive"] = {
+    loaded = true,
+    path = "/home/hisbil/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    url = "https://github.com/tpope/vim-fugitive"
+  },
   ["vim-unimpaired"] = {
     loaded = true,
     path = "/home/hisbil/.local/share/nvim/site/pack/packer/start/vim-unimpaired",

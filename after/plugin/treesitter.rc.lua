2436e89351c339bf57947c4969f56d4aaa6dd456
hisbil islami <hisbil.prog@gmail.com>
2024-07-03 10:44:37 +0700

    blade treesitter

--- after/plugin/treesitter.rc.lua
+++ after/plugin/treesitter.rc.lua
@@ -30,4 +30,14 @@ treesitter.setup {
 }
 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.blade = {
+  install_info = {
+    url = "https://github.com/EmranMR/tree-sitter-blade",
+    files = { "src/parser.c" },
+    branch = "main",
+  },
+  filetype = "blade"
+}
+parser_config.blade.filetype_to_parsername = { "blade", "html" }
+
 parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }

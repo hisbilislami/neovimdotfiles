c8411e807c36793141d45b49eea0c37e85388ab3
hisbil islami <hisbil.prog@gmail.com>
2024-09-23 08:33:59 +0700

    migrate tsserver to ts_ls because tsserver would be deprecated in the next lsp version

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/hisbil/.cache/nvim/packer_hererocks/2.1.1713484068/share/lua/5.1/?.lua;/home/hisbil/.cache/nvim/packer_hererocks/2.1.1713484068/share/lua/5.1/?/init.lua;/home/hisbil/.cache/nvim/packer_hererocks/2.1.1713484068/lib/luarocks/rocks-5.1/?.lua;/home/hisbil/.cache/nvim/packer_hererocks/2.1.1713484068/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/hisbil/.cache/nvim/packer_hererocks/2.1.1713484068/lib/lua/5.1/?.so"
+local package_path_str = "/home/hisbil/snap/alacritty/common/.cache/nvim/packer_hererocks/2.1.1703358377/share/lua/5.1/?.lua;/home/hisbil/snap/alacritty/common/.cache/nvim/packer_hererocks/2.1.1703358377/share/lua/5.1/?/init.lua;/home/hisbil/snap/alacritty/common/.cache/nvim/packer_hererocks/2.1.1703358377/lib/luarocks/rocks-5.1/?.lua;/home/hisbil/snap/alacritty/common/.cache/nvim/packer_hererocks/2.1.1703358377/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/hisbil/snap/alacritty/common/.cache/nvim/packer_hererocks/2.1.1703358377/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -228,6 +228,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/hisbil/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
+  },
+  ["vim-mustache-handlebars"] = {
+    loaded = true,
+    path = "/home/hisbil/.local/share/nvim/site/pack/packer/start/vim-mustache-handlebars",
+    url = "https://github.com/mustache/vim-mustache-handlebars"
   },
   ["vim-unimpaired"] = {
     loaded = true,
